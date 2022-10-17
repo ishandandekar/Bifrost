ddc5cce71192b2744d4491e17c2fe198e5517dcf
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-10-17 16:09:25 +0530

    Update UI

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 from script import Graph
-print("Loading the object...")
 
 
 @st.cache(allow_output_mutation=True)
@@ -34,6 +33,18 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("""
-    ## About the project
+    ## Introduction
 
+**BiFrost** is an AI application, which gives an optimal route between two places, based on the time taken for the journey. The algorithm for path ooptimization, is A-star search algorithm. The idea behind this project is to give the user better transportation options, when travelling using public transport. For example, if a person wants to go from Point A to Point B, you have multiple options. The algorithm then calculates a path considering the time taken for the journey to get the best path.  
+In the early development stages, we used Mumbai(Maharashtra, India) and Navi Mumbai(Maharashtra, India). We got this data using the famous mobile app **M-Indicator**. The app shows real time details about train timings, which we felt was the best way to retrieve the data.  
+This network of railway, is imitated as a **Graph** data structure. This made it easier to add the nodes (or stops/destinations). Further, if there is any development on creating better lines for transportation, adding to the network will be very easy.
+We did this project as a part of our bachelor's degree for the Applied AI subject.
+
+## Contributions
+
+This project has been commenced under the [MIT License](LICENSE). Thus, making it open source.  
+The project is still in its development phases and the developer(s) are always looking for improvements. If you want to contribute to this project in any sort of, please make a pull-request with the improved code and/or documentation.  
+If you are interested in seeing the project grow, please star the project, it will be really motivating for us!
     """)
+    st.markdown("""
+    #### [Github](https://github.com/ishandandekar/Bifrost)""")
