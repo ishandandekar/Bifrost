cc7fdfa4c6f39e64307c91460bbf23a9e60701d1
Ishan Dandekar <ishandandekar2002@gmail.com>
2022-11-03 19:19:33 +0530

    Add new algo

--- scripts/script.py
+++ scripts/script.py
@@ -95,71 +95,8 @@ class Graph:
         H = {vertex: 1 for vertex in self.vertices}
         return H[node]
 
-    def a_star_algorithm(self, start: str, end: str) -> Union[Tuple[List[str], int], None]:
-        """
-        Performs A-star search algorithm to find the shortest path
-
-        Args:
-            start (str): Represents the start of the journey
-            end (str): Represents the destination of the journey
-
-        Returns:
-            Union[Tuple[List[str], int], None]: Returns the path (as a list) and the time taken to traverse the path, or returns None, if no path is found
-        """
-        open_list = set([start])
-        closed_list = set([])
-        g = {}
-
-        g[start] = 0
-        parents = {}
-        parents[start] = start
-
-        while len(open_list) > 0:
-            n = None
-            for v in open_list:
-                if n == None or g[v] + self._h(v) < g[n] + self._h(n):
-                    n = v
-
-            if n == None:
-                return None
-
-            if n == end:
-                reconst_path = []
-
-                while parents[n] != n:
-                    reconst_path.append(n)
-                    n = parents[n]
-
-                reconst_path.append(end)
-
-                reconst_path.reverse()
-                reconst_path[0] = start
-
-                time_duration = 0
-                for node in reconst_path:
-                    if node == end:
-                        break
-                    for neighbour, time in self._get_adjacency_list(node):
-                        if neighbour == reconst_path[reconst_path.index(node) + 1]:
-                            time_duration += time
-                return reconst_path, time_duration
-            for (m, weight) in self._get_adjacency_list(n):
-                if m not in open_list and m not in closed_list:
-                    open_list.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight
-                else:
-                    if g[m] > g[n] + weight:
-                        g[m] = g[n] + weight
-                        parents[m] = n
-
-                        if m in closed_list:
-                            closed_list.remove(m)
-                            open_list.add(m)
-            open_list.remove(n)
-            closed_list.add(n)
-
-        return None
+    def a_star_algorithm(self, start: str, end: str):
+        pass
 
 
 if __name__ == '__main__':
